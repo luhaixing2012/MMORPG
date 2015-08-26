a3451c9cb7490245c0264d2cbb7c8e2b4702478e
luhaixing2012 <luhaixing2012@163.com>
2015-08-26 20:56:16 +0800

    InPlayer类型动画的运动
    InPlayer类型动画的运动。区别于自带Root Motion的动画

---
+++ Project/Assets/RootController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RootController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnAnimatorMove()
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+
+        if (animator)
+        {
+            Vector3 pos = gameObject.transform.position;
+            pos.z += animator.GetFloat("Speed") * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
